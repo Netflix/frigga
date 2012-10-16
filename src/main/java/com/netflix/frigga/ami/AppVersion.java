fc19e470a9d284cc4575edc16d10f39e955b8734
Jason Gritman <jasongritman@gmail.com>
2012-10-15 17:27:28 -0700

    add ami classes

---
+++ src/main/java/com/netflix/frigga/ami/AppVersion.java
@@ -0,0 +1,107 @@
+package com.netflix.frigga.ami;
+
+import com.netflix.frigga.NameConstants;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AppVersion implements Comparable {
+
+    /**
+     * All of these are valid:
+     * subscriberha-1.0.0-586499
+     * subscriberha-1.0.0-586499.h150
+     * subscriberha-1.0.0-586499.h150/WE-WAPP-subscriberha/150
+     */
+    private static final Pattern APP_VERSION_PATTERN = Pattern.compile(
+            "([" + NameConstants.NAME_HYPHEN_CHARS +
+            "]+)-([0-9.]+)-([0-9]{5,7})(?:[.]h([0-9]+))?(?:\\/([-a-zA-z0-9]+)\\/([0-9]+))?");
+
+
+    private String packageName;
+    private String version;
+    private String buildJobName;
+    private String buildNumber;
+    private String changelist;
+
+    private AppVersion() {
+    }
+
+    public static AppVersion parseName(String amiName) {
+        Matcher matcher = APP_VERSION_PATTERN.matcher(amiName);
+        if (!matcher.matches()) {
+            return null;
+        }
+        AppVersion parsedName = new AppVersion();
+        parsedName.packageName = matcher.group(1);
+        parsedName.version = matcher.group(2);
+        parsedName.changelist = matcher.group(3);
+        parsedName.buildNumber = matcher.group(4);
+        parsedName.buildJobName = matcher.group(5);
+        return parsedName;
+    }
+
+    public int compareTo(Object o) {
+        if (equals (o)) { // if x.equals(y), then x.compareTo(y) should be 0
+            return 0;
+        }
+
+        if (o == null) {
+            return 1; // equals(null) can never be true, so compareTo(null) should never be 0
+        }
+
+        AppVersion other = (AppVersion) o; // ClassCastException is the desired result here
+
+        int comparison = packageName.compareTo(other.packageName);
+        if (comparison != 0) {
+            return comparison;
+        }
+        comparison = version.compareTo(other.version);
+        if (comparison != 0) {
+            return comparison;
+        }
+        comparison = buildJobName.compareTo(other.buildJobName);
+        if (comparison != 0) {
+            return comparison;
+        }
+        comparison = buildNumber.compareTo(other.buildNumber);
+        if (comparison != 0) {
+            return comparison;
+        }
+        comparison = changelist.compareTo(other.changelist);
+        return comparison;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("AmiName [packageName=").append(packageName).append(", version=").append(version)
+                .append(", buildJobName=").append(buildJobName).append(", buildNumber=").append(buildNumber)
+                .append(", changelist=").append(changelist).append("]");
+        return builder.toString();
+    }
+
+    public static Pattern getAppVersionPattern() {
+        return APP_VERSION_PATTERN;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getBuildJobName() {
+        return buildJobName;
+    }
+
+    public String getBuildNumber() {
+        return buildNumber;
+    }
+
+    public String getChangelist() {
+        return changelist;
+    }
+
+}
---
+++ src/main/java/com/netflix/frigga/ami/BaseAmiInfo.java
@@ -0,0 +1,81 @@
+package com.netflix.frigga.ami;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BaseAmiInfo {
+
+    private static final String IMAGE_ID = "ami-[a-z0-9]{8}";
+    private static final Pattern BASE_AMI_ID_PATTERN = Pattern.compile("^.*?base_ami_id=(" + IMAGE_ID + ").*?");
+    private static final Pattern ANCESTOR_ID_PATTERN = Pattern.compile("^.*?ancestor_id=(" + IMAGE_ID + ").*?$");
+    private static final Pattern BASE_AMI_NAME_PATTERN = Pattern.compile("^.*?base_ami_name=([^,]+).*?$");
+    private static final Pattern ANCESTOR_NAME_PATTERN = Pattern.compile("^.*?ancestor_name=([^,]+).*?$");
+    private static final Pattern AMI_DATE_PATTERN = Pattern.compile(".*\\-(20[0-9]{6})(\\-.*)?");
+
+    private String baseAmiId;
+    private String baseAmiName;
+    private Date baseAmiDate;
+
+    private BaseAmiInfo() {}
+
+    public static BaseAmiInfo parseDescription(String imageDescription) {
+        BaseAmiInfo info = new BaseAmiInfo();
+        info.baseAmiId = extractBaseAmiId(imageDescription);
+        info.baseAmiName = extractBaseAmiName(imageDescription);
+        if (info.baseAmiName != null) {
+            Matcher dateMatcher = AMI_DATE_PATTERN.matcher(info.baseAmiName);
+            if (dateMatcher.matches()) {
+                try {
+                    // Example: 20100823
+                    info.baseAmiDate = new SimpleDateFormat("yyyyMMdd").parse(dateMatcher.group(1));
+                } catch (Exception ignored) {
+                    // Ignore failure.
+                }
+            }
+        }
+        return info;
+    }
+
+    private static String extractBaseAmiId(String imageDescription) {
+        // base_ami_id=ami-1eb75c77,base_ami_name=servicenet-roku-qadd.dc.81210.10.44
+        Matcher matcher = BASE_AMI_ID_PATTERN.matcher(imageDescription);
+        if (matcher.matches()) {
+            return matcher.group(1);
+        }
+        // store=ebs,ancestor_name=ebs-centosbase-x86_64-20101124,ancestor_id=ami-7b4eb912
+        matcher = ANCESTOR_ID_PATTERN.matcher(imageDescription);
+        if (matcher.matches()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    private static String extractBaseAmiName(String imageDescription) {
+        // base_ami_id=ami-1eb75c77,base_ami_name=servicenet-roku-qadd.dc.81210.10.44
+        Matcher matcher = BASE_AMI_NAME_PATTERN.matcher(imageDescription);
+        if (matcher.matches()) {
+            return matcher.group(1);
+        }
+        // store=ebs,ancestor_name=ebs-centosbase-x86_64-20101124,ancestor_id=ami-7b4eb912
+        matcher = ANCESTOR_NAME_PATTERN.matcher(imageDescription);
+        if (matcher.matches()) {
+            return matcher.group(1);
+        }
+        return null;
+    }
+
+    public String getBaseAmiId() {
+        return baseAmiId;
+    }
+
+    public String getBaseAmiName() {
+        return baseAmiName;
+    }
+
+    public Date getBaseAmiDate() {
+        return baseAmiDate;
+    }
+
+}
